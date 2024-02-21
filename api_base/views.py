cf3e240f849cb6b0d145e15c6e4fa301d45fd0c3
darko <ki14.skakun@gmail.com>
2024-02-21 18:27:09 +0200

    Authentication examples

--- api_base/views.py
+++ api_base/views.py
@@ -8,6 +8,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .models import Article
 from .serializers import ArticleSerializer
 
@@ -15,10 +17,14 @@ from .serializers import ArticleSerializer
 class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin,
                      mixins.CreateModelMixin, mixins.UpdateModelMixin,
                      mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
 
     lookup_field = 'id'
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, id=None):
         if id:
