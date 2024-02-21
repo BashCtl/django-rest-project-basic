2bb9ab40ff3c1899c1ca24e12be9fd233fe75524
darko <ki14.skakun@gmail.com>
2024-02-21 16:33:27 +0200

    article details

--- api_base/views.py
+++ api_base/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.parsers import JSONParser
 from .models import Article
 from .serializers import ArticleSerializer
 
 
+@csrf_exempt
 def article_list(request):
     if request.method == 'GET':
         articles = Article.objects.all()
@@ -19,3 +21,28 @@ def article_list(request):
             article_serializer.save()
             return JsonResponse(article_serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(article_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
+def article_detail(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+
+    except Article.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        article_serializer = ArticleSerializer(article)
+        return JsonResponse(article_serializer.data, safe=False)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        article_serializer = ArticleSerializer(article, data=data)
+        if article_serializer.is_valid():
+            article_serializer.save()
+            return JsonResponse(article_serializer.data)
+        return JsonResponse(article_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        article.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
